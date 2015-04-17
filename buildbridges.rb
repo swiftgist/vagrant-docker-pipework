455bdf5c8d837df91ea599acddd3fce982fb481b
Eric Jackson <swiftgist@gmail.com>
2015-04-17 15:06:02 -0400

    Add missing steps, remove redundancy

--- buildbridges.rb
+++ buildbridges.rb
@@ -3,6 +3,7 @@
 require 'pp'
 require 'open3'
 require 'yaml'
+require 'fileutils'
 
 # The namespace for holding Containers, Bridges and Bridge classes.
 #
@@ -12,6 +13,19 @@ require 'yaml'
 #   Bridges.new(all_bridge_details, container_id)
 #   Bridge(name, bridge_details)
 module Buildable
+
+  # Execute a command, print both stdout and stderr, and exit unless successful
+  #
+  #   cmd - a string of the command
+  def self.popen(cmd)
+    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+      puts stdout.readlines
+      line = stderr.readlines.join
+      puts line
+      exit unless (wait_thr.value.success? or 
+                   line.match(/RTNETLINK answers: File exists/))
+    end
+  end
 
   # Docker uses LXC (i.e. Linux Containers).  These containers represent each 
   # machine that needs one or more bridges.
@@ -61,13 +75,7 @@ module Buildable
 
           cmd = "sudo #{PIPEWORK} br_#{bridge} -i #{@config['bridges'][bridge]['device']} #{@containers[server]} #{@config['bridges'][bridge]['base']}.#{@config['servers'][server]['octet']}/24"
           puts cmd
-          Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-            puts stdout.readlines
-            line = stderr.readlines.join
-            puts line
-            exit unless (wait_thr.value.success? or 
-                         line.match(/RTNETLINK answers: File exists/))
-          end
+          Buildable::popen(cmd)
           puts
         end
       end
@@ -78,7 +86,7 @@ module Buildable
     # builds the interfaces.
     def build
       @containers.keys.each do |container|
-        b = Buildable::Bridges.new(@config['bridges'], @containers[container])
+        b = Buildable::Bridges.new(@config, container, @containers[container])
         b.build
       end
     end
@@ -94,12 +102,12 @@ module Buildable
     #
     #   all_bridges - a hash of bridge names and associated settings
     #   container_id - 12 character identity of a docker container
-    def initialize(all_bridges, container_id)
+    def initialize(config, container, container_id)
       @bridges = []
       @veth = {}
-      all_bridges.keys.each do |name|
-        @bridges << Bridge.new(name, all_bridges[name])
-        @veth[name] = Veth.new(container_id, name, all_bridges[name])
+      config['bridges'].keys.each do |bridge_name|
+        @bridges << Bridge.new(bridge_name, config['bridges'][bridge_name])
+        @veth[bridge_name] = Veth.new(config['servers'][container]['octet'], container_id, bridge_name, config['bridges'][bridge_name])
       end
       @container_id = container_id
     end
@@ -114,11 +122,15 @@ module Buildable
           bridge.up
         end
         @veth[bridge.name].mtu = bridge.mtu
+        @veth[bridge.name].symlink
         if @veth[bridge.name].add then
           @veth[bridge.name].set
           @veth[bridge.name].up
           @veth[bridge.name].link
-          @veth[bridge.name].netns
+          @veth[bridge.name].netns_link
+          @veth[bridge.name].netns_up
+          @veth[bridge.name].netns_add
+          @veth[bridge.name].netns_arp
         end
 
       end
@@ -145,14 +157,14 @@ module Buildable
     def add
       cmd = "sudo ip link add dev #{@name} type bridge"
       pp cmd
-      popen(cmd)
+      Buildable::popen(cmd)
     end
 
     # Print the command and enable the bridge interface
     def up
       cmd = "sudo ip link set #{@name} up"
       pp cmd
-      popen(cmd)
+      Buildable::popen(cmd)
     end
 
     # Print the command and return the mtu value for the bridge interface
@@ -170,16 +182,6 @@ module Buildable
       File.exists?("/sys/class/net/#{@name}")
     end
 
-    # Execute a command, print both stdout and stderr, and exit unless successful
-    #
-    #   cmd - a string of the command
-    def popen(cmd)
-      Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-        puts stdout.readlines
-        puts stderr.readlines
-        exit unless wait_thr.value.success?
-      end
-    end
 
   end
 
@@ -194,11 +196,12 @@ module Buildable
     #   container_id = 12 character identifier for a Docker container
     #   name - The name of the bridge
     #   settings - The bridge settings
-    def initialize(container_id, name, settings)
+    def initialize(octet, container_id, bridge_name, settings)
       @container_id = container_id
-      @name = name
+      @bridge_name = bridge_name
       @docker_pid = dockerpid
       @container_ifname = settings['device']
+      @address = "#{settings['base']}.#{octet}"
       @local_ifname = "v#{@container_ifname}pl#{@docker_pid}"
       @guest_ifname = "v#{@container_ifname}pg#{@docker_pid}"
       @mtu = nil
@@ -214,25 +217,37 @@ module Buildable
       end
     end
 
+    # Print commands and recreate symlink from /proc to /var/run
+    def symlink
+      symlink_path = "/var/run/netns/#{@docker_pid}"
+      cmd = "sudo rm -f #{symlink_path}"
+      pp cmd
+      Buildable::popen(cmd)
+
+      cmd = "sudo ln -s /proc/#{@docker_pid}/ns/net #{symlink_path}"
+      pp cmd
+      Buildable::popen(cmd)
+    end
+    
     # Prints command and add veth peers
     def add
       cmd = "sudo ip link add name #{@local_ifname} mtu #{@mtu} type veth peer name #{@guest_ifname} mtu #{@mtu}"
       pp cmd
-      popen(cmd)
+      Buildable::popen(cmd)
     end
 
     # Prints command and assigns the bridge master device
     def set
-      cmd = "sudo ip link set #{@local_ifname} master #{@name}"
+      cmd = "sudo ip link set #{@local_ifname} master #{@bridge_name}"
       pp cmd
-      popen(cmd)
+      Buildable::popen(cmd)
     end
 
     # Prints command and enables local interface
     def up
       cmd = "sudo ip link set #{@local_ifname} up"
       pp cmd
-      popen(cmd)
+      Buildable::popen(cmd)
 
     end
 
@@ -240,26 +255,36 @@ module Buildable
     def link
       cmd = "sudo ip link set #{@guest_ifname} netns #{@docker_pid}"
       pp cmd
-      popen(cmd)
+      Buildable::popen(cmd)
     end
 
     # Prints command and sets the guest interface to the container interface (i.e.
     # bridge name on host) from within the network namespace of the guest.
-    def netns
+    def netns_link
       cmd = "sudo ip netns exec #{@docker_pid} ip link set #{@guest_ifname} name #{@container_ifname}"
       pp cmd
-      popen(cmd)
+      Buildable::popen(cmd)
     end
 
-    # Redundant
-    def popen(cmd)
-      Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-        puts stdout.readlines
-        line = stderr.readlines.join
-        puts line
-        wait_thr.value.success?
-                    # line.match(/RTNETLINK answers: File exists/))
-      end
+    # Prints command and assigns address to interface
+    def netns_add
+      cmd = "sudo ip netns exec #{@docker_pid} ip addr add #{@address}/24 dev #{@container_ifname}"
+      pp cmd
+      Buildable::popen(cmd)
+    end
+
+    # Prints command and enables interface on guest
+    def netns_up
+      cmd = "sudo ip netns exec #{@docker_pid} ip link set #{@container_ifname} up"
+      pp cmd
+      Buildable::popen(cmd)
+    end
+
+    # Prints command and notifies bridge
+    def netns_arp
+      cmd = "sudo ip netns exec #{@docker_pid} arping -c 1 -A -I #{@container_ifname} #{@address}"
+      pp cmd
+      Buildable::popen(cmd)
     end
 
   end
